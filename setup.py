d06fbf3e9ac16e397c708cb21d4f35b1cdc88871
hellmanj <hellmanj@ncbi.nlm.nih.gov>
2016-02-14 15:41:44 -0500

    update setup

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-protractor',
-    version='0.8.1',
+    name='django-protractor-new',
+    version='0.9.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django>=1.4'],
     license='MIT License',  # example license
     description='Easily integrate your protractor tests with django',
     long_description=README,
-    author='James Pulec',
-    author_email='jpulec@gmail.com',
+    author='Jared Hellman (formerly James Pulec)',
+    author_email='jhellma1@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
